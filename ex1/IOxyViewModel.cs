ef099c802e7c99c5f1280601b75546a9f905a796
EhudVasserman <EhudVasserman@hotmail.com>
2021-04-08 18:25:22 +0300

    IOxyViewModel added

---
+++ ex1/IOxyViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DLL;
+using System.ComponentModel;
+
+using OxyPlot.Series;
+using OxyPlot;
+using OxyPlot.Axes;
+namespace ex1
+{
+    public interface IOxyViewModel : INotifyPropertyChanged
+    {
+
+        // model: graphModel: SpecialPoint -> 
+        // view-model: IOxyViewModel: LineSeries ->
+        // view :MainWindow -> SetUp(oxyViewModel)
+        LineSeries Ls { get; }
+        //in the IMPLEMENTATION : private IAnomalyGraphModel graphModel;
+        ScatterSeries Scs { get; }
+        LinearAxis BottomAxis { get; }
+        LinearAxis LeftAxis { get; }
+        String LegendTitle { get; }
+
+        // INotifyPropertyChanged:
+        // event PropertyChangedEventHandler PropertyChanged;
+
+    }
+}
